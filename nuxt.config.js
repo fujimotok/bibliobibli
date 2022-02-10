30498315a2e29b157fbcd4b1348ce2c85e43964f
fj <fj@Viajero>
2022-02-10 22:59:11 +0900

    fix pwa settings.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,28 @@
 import colors from 'vuetify/es5/util/colors'
+import path from 'path'
+import fs from 'fs'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - bibliobibli',
-    title: 'bibliobibli',
+    script: [
+      {
+        src:
+        "https://cdn.jsdelivr.net/npm/pwacompat@2.0.6/pwacompat.min.js",
+        integrity:
+        "sha384-GOaSLecPIMCJksN83HLuYf9FToOiQ2Df0+0ntv7ey8zjUHESXhthwvq9hXAZTifA",
+        crossorigin: "anonymous",
+        defer:true //ここ
+      }
+    ],
+    titleTemplate: '%s - BIBLIoBIBLI',
+    title: 'BIBLIoBIBLI',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'mobile-web-app-capable', content: 'yes' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -44,7 +57,24 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      name: 'BIBLIoBIBLI',
+      lang: 'ja',
+      description: '説明',
+      orientation: 'portrait', scope: '/',
+      display: 'standalone',
+      id: '/',
+      start_url: '/',
+      theme_color: '#3C6A0F',
+      background_color: '#FFFFFF',
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
     }
   },
 
@@ -52,7 +82,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: '#3C6A0F',
