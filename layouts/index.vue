6fedb9bcfcef1da77b6c09e4ac05140879bdd603
fj <fj@Viajero>
2022-02-12 11:34:53 +0900

    Add actionmenu in index-page layout.

---
+++ layouts/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <default-layout ref="base">
+    <v-btn color="white" icon @click="click()">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+  </default-layout>
+</template>
+
+<script>
+import defaultLayout from '~/layouts/default.vue'
+
+export default {
+  name: 'IndexLayout',
+  components: { defaultLayout },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    click () {
+      this.$refs.base.$refs.page.$children[0].add()
+    }
+  }
+}
+</script>
