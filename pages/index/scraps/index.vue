5f3f473337fd16035ca52049b1b2fbd51dbc62bc
fj <fj@Viajero>
2022-06-26 23:00:02 +0900

    Fix scrap pages.

--- pages/index/scraps/index.vue
+++ pages/index/scraps/index.vue
@@ -1,17 +1,114 @@
 <template>
-  <v-container fluid style="height: 100%; background: #808080;">
-    <v-row justify="center" align="center">
-      <v-col>
-        <v-card class="logo py-4 d-flex justify-center">
-          <p>scraps index</p>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <vue-simplemde ref="markdownEditor" v-model="scrap.content" :configs="config"/>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'ScrapsIndexPage'
-}
+<script lang="ts">
+import Vue from 'vue'
+import { ScrapRepository } from '../../../js/db/interfaces/ScrapRepository'
+
+export default Vue.extend({
+  name: 'ScrapsIndexPage',
+  data: () => ({
+    config: {
+      spellChecker: false,
+      forceSync: true,
+      indentWithTabs: false,
+      status: false,
+      toolbar: [
+        'bold',
+        'strikethrough',
+        '|',
+        'heading',
+        'unordered-list',
+        'ordered-list',
+        '|',
+        'code',
+        'quote',
+        '|',
+        'link',
+        {
+          name: 'image',
+          action: (editor) => {
+            const self = this
+            this.$refs.fileInput.$refs.input.addEventListener('change', function onChange () {
+              self.$refs.fileInput.$refs.input.removeEventListener('change', onChange)
+              const fileReader = new FileReader()
+              fileReader.onload = function () {
+                const dataURI = this.result
+                const cm = editor.codemirror
+                const pos = cm.getCursor('start')
+                cm.replaceRange('![](' + dataURI + ')', { line: pos.line, ch: 0 })
+              }
+              if (self.file) {
+                fileReader.readAsDataURL(self.file)
+              }
+            })
+            this.$refs.fileInput.$refs.input.click()
+          },
+          className: 'fa fa-image',
+          title: 'image'
+        },
+        '|',
+        'preview'
+      ]
+    },
+    scrap: {
+      id: null,
+      createdAt: '',
+      updatedAt: '',
+      content: '',
+      path: '',
+    }
+  }),
+  beforeMount () {
+    this.$store.commit('CHANGE_IS_SHOW_SAVE', true)
+    this.$store.commit('CHANGE_IS_SHOW_DEL', false)
+  },
+  mounted () {
+    window.addEventListener('resize', this.resize)
+    this.resize()
+  },
+  methods: {
+    resize () {
+      const toolbarHeight = document.querySelector('.editor-toolbar').clientHeight
+      const height = window.visualViewport.height - (48 + 16 * 2 + toolbarHeight)
+      document.querySelector('.CodeMirror').style.height = height + 'px'
+      window.scroll(0, 0)
+    },
+    save () {
+      const scrapRepo: ScrapRepository = this.$scrapRepository
+      scrapRepo.store(this.scrap)
+    }
+  }
+})
 </script>
+
+<style>
+.CodeMirror .CodeMirror-code .cm-header-1 {
+    font-size: 110%;
+    line-height: 110%;
+}
+
+.CodeMirror .CodeMirror-code .cm-header-2 {
+    font-size: 110%;
+    line-height: 110%;
+}
+
+.CodeMirror .CodeMirror-code .cm-header-3 {
+    font-size: 110%;
+    line-height: 110%;
+}
+
+.CodeMirror .CodeMirror-code .cm-header-4 {
+    font-size: 110%;
+    line-height: 110%;
+}
+
+.CodeMirror .CodeMirror-code .cm-comment {
+    background: rgba(0, 0, 255, .1);
+    border-radius: 2px;
+}
+</style>
+
