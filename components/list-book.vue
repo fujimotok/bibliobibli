d6f1a448c7d22f472ff6ef3ade33cd26286cef40
fj <fj@Viajero>
2022-07-21 23:16:07 +0900

    Add book search func.

--- components/list-book.vue
+++ components/list-book.vue
@@ -4,6 +4,7 @@
   >
     <template v-for="(item, index) in items">
       <v-list-item :key="index" @click="show(item.id)">
+        <div v-if="index === items.length - 1" v-intersect.quiet="onIntersect" />
         <v-list-item-content>
           <v-list-item-title v-text="item.title" />
           <v-list-item-subtitle
@@ -36,7 +37,94 @@
       </v-list-item>
       <v-divider :key="`${index}-divider`" />
     </template>
-    <div v-intersect.quite="onIntersect" /> 
+    <v-dialog v-model="dialog" max-width="400">
+      <v-card class="pa-4">
+        <v-card-title>
+          Search
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            ref="input"
+            v-model="searchWord"
+            hide-details
+            outlined
+            prepend-inner-icon="mdi-magnify"
+            single-line
+            placeholder="Search in Title or ISBN"
+            class="mb-4"
+            @keydown.enter="onEnter"
+          />
+          <v-select
+            v-model="searchStates"
+            multiple
+            return-object
+            :items="states"
+            label="states"
+            :menu-props="{ offsetY: true }"
+          >
+            <template #prepend-item>
+              <v-list-item
+                ripple
+                @mousedown.prevent
+                @click="selectAllStates"
+              >
+                <v-list-item-action>
+                  <v-icon :color="searchStates.length > 0 ? 'primary' : ''">
+                    {{ searchStatesIcon }}
+                  </v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Select All
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider class="mt-2" />
+            </template>
+          </v-select>
+          <v-select
+            v-model="searchTags"
+            multiple
+            :items="tagItems"
+            label="tags"
+            item-value="id"
+            item-text="name"
+            :menu-props="{ offsetY: true }"
+          >
+            <template #prepend-item>
+              <v-list-item
+                ripple
+                @mousedown.prevent
+                @click="selectAllTags"
+              >
+                <v-list-item-action>
+                  <v-icon :color="searchTags.length > 0 ? 'primary' : ''">
+                    {{ searchTagsIcon }}
+                  </v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Select All
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider class="mt-2" />
+            </template>
+          </v-select>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn @click="clear()">
+            <v-icon>mdi-filter-variant-remove</v-icon>
+            クリア
+          </v-btn>
+          <v-btn class="primary" @click="find(); dialog = false">
+            <v-icon>mdi-magnify</v-icon>
+            検索
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-list-item-group>
 </template>
 
@@ -49,8 +137,11 @@ export type DataType = {
   items: object[]
   tagItems: Tag[]
   searchWord: string
-  searchTags: Tag[]
+  searchTags: number[]
+  states: object[],
+  searchStates: number[],
   isLoading: boolean
+  dialog: boolean
   status: object
 }
 
@@ -64,7 +155,17 @@ export default Vue.extend({
       tagItems: [],
       searchWord: '',
       searchTags: [],
+      states: [
+        { text: '読みたい', value: 0, icon: 'mdi-progress-star' },
+        { text: '未読', value: 1, icon: 'mdi-progress-clock' },
+        { text: '読中', value: 2, icon: 'mdi-progress-check' },
+        { text: '読了', value: 3, icon: 'mdi-check' }
+      ],
+      searchStates: [
+        0, 1, 2, 3
+      ],
       isLoading: false,
+      dialog: false,
       status: Status
     }
   },
@@ -76,6 +177,28 @@ export default Vue.extend({
       set (value) {
         this.$emit('input', value)
       }
+    },
+    searchAllTags (): boolean {
+      return this.searchTags.length === this.tagItems.length
+    },
+    searchSomeTags (): boolean {
+      return this.searchTags.length > 0 && !this.searchAllTags
+    },
+    searchTagsIcon (): string {
+      if (this.searchAllTags) { return 'mdi-close-box' }
+      if (this.searchSomeTags) { return 'mdi-minus-box' }
+      return 'mdi-checkbox-blank-outline'
+    },
+    searchAllStates (): boolean {
+      return this.searchStates.length === this.states.length
+    },
+    searchSomeStates (): boolean {
+      return this.searchStates.length > 0 && !this.searchAllStates
+    },
+    searchStatesIcon (): string {
+      if (this.searchAllStates) { return 'mdi-close-box' }
+      if (this.searchSomeStates) { return 'mdi-minus-box' }
+      return 'mdi-checkbox-blank-outline'
     }
   },
   async mounted () {
@@ -85,18 +208,22 @@ export default Vue.extend({
       this.tagItems = tags[0]
     }
     
-    this.search()
+    await this.find()
   },
   methods: {
     show (id: number) {
       this.$router.push({ path: '/books/' + id })
     },
-    async search (offset = 0) {
+    async find (offset = 0) {
+      if (offset === 0) {
+        this.items = []
+      }
+
       const bookRepo: BookRepository = this.$bookRepository
-      const books = await bookRepo.find(this.searchWord, [0,1,2,3], [], offset, 20)
+      const books = await bookRepo.find(this.searchWord, this.searchStates, this.searchTags, offset, 20)
       if (books !== undefined) {
-        books[0].forEach((book) => {
-          this.items.push({
+        const res = books[0].map((book) => {
+          return {
             id: book.id,
             title: book.title,
             authors: book.authors,
@@ -105,14 +232,56 @@ export default Vue.extend({
               const elem = this.tagItems.find((t) => t.id === tag)
               return elem?.name
             })
-          })
+          }
         })
+        
+        this.items = this.items.concat(res)
       }
     },
-    onIntersect () {
+    async onIntersect () {
       this.isLoading = true
-      this.search(this.items.length)
+      await this.find(this.items.length)
       this.isLoading = false
+    },
+    search () {
+      this.dialog = true
+    },
+    selectAllStates () {
+      this.$nextTick(() => {
+        if (this.searchAllStates) {
+          this.searchStates = []
+        } else {
+          this.searchStates = this.states.map((s: any) => s.value as number)
+        }
+      })
+    },
+    selectAllTags () {
+      this.$nextTick(() => {
+        if (this.searchAllTags) {
+          this.searchTags = []
+        } else {
+          this.searchTags = this.tagItems.slice().map(t => t.id || 0)
+        }
+      })
+    },
+    clear () {
+      this.searchWord = ''
+      this.searchStates = [0, 1, 2, 3]
+      this.searchTags = []
+
+      this.find()
+      this.dialog = false
+      window.scrollTo({ top: 0 })
+    },
+    onEnter (event: any) {
+      if (event.keyCode !== 13) {
+        return
+      }
+      const input = this.$refs.input as HTMLElement
+      input.blur() // hide software keyboard
+      this.find()
+      this.dialog = false
+      window.scrollTo({ top: 0 })
     }
   }
 })
